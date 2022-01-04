c9b53a9c49862da7e2dfb37d1bd01eab9f453ae7
Yash-Haque <58705635+Yash-Haque@users.noreply.github.com>
2022-01-04 15:53:35 +0600

    Update hotel.js

--- server/routes/hotel.js
+++ server/routes/hotel.js
@@ -1,23 +1,41 @@
 import express from 'express';
 import formidable from "express-formidable";
 
+// Places that require sigin are assigned the middleware function requireSignin.
+// And the following function always goes first as a parameter before the others.
+// Check middlewares folder for more info.  
 
 
 const router = express.Router();
 
 // Middleware
-// import { requireSignin } from "../middlewares";
+// import { requireSignin, hotelOwner } from "../middlewares";
 
 // Controllers
-import { create, hotels, image } from "../controllers/hotel";
+import { create, hotels, image, sellerHotels, remove, read, update, userHotelBookings } from "../controllers/hotel";
 
 // router.get("/:message", showMessage);
 
-router.post("/create-hotel", formidable(), create);
+router.post("/create-hotel", formidable(), create); //requireSignin
 
 router.get("/hotels", hotels);
 
 router.get("/hotel/image/:hotelID", image);
+
+router.get("/seller-hotels", sellerHotels); //requireSignin
+
+// First hotelOwner then requireSignin
+router.delete("/delete-hotel/:hotelId", remove);
+
+router.get("/hotel/:hotelId", read);
+
+//first requireSignin then hotelowner
+router.put("/update-hotel/:hotelId", formidable(), update);
+
+
+// Orders
+//first requireSignin
+router.get("/user-hotel-bookings", userHotelBookings);
 
 // Since we used require in middleware, hence the following cmd won't 
 // support
