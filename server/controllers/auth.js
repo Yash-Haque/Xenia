6df8e40dd5dff9122b47a33c7a670008521af138
Yash-Haque <58705635+Yash-Haque@users.noreply.github.com>
2022-01-04 15:56:25 +0600

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,31 +1,40 @@
 import User from "../models/user";
+
 import jsonwebtoken, { JsonWebTokenError } from "jsonwebtoken";
-// export const showMessage =  (req, res) => {
-//     res.status(200).send(`Here is your message: ${req.params.message}`);
-// }
+
+// A lot of the function attributes in the following controllers have been removed
+// due to suit the needs.
 
 export const register = async (req, res) => {
-    console.log(req.body);
-    const {firstName, lastName, phoneNumber, email, password,
-         confirmPassword} = req.body;
-    //validation
-    if(!firstName && !lastName) return res.status(400).send("Name is required");
-    if(!password || password.length < 6)
-        return res
-            .status(400)
-            .send("Password is required and should be min 6 characters long");
-    if(!(password == confirmPassword)) 
-        return res
-        .status(400).send("Passwords don't match. Please try again.");
-        let userExist = await User.findOne({email}).exec();
-        if(userExist) return res.status(400).send("Email is taken");
-        let phoneNumberExist = await User.findOne({phoneNumber}).exec();
-            if(phoneNumberExist) return res.status(400).send("An account with the following phone number already exists");
-            //register
-            const user = new User(req.body)
+
             try {
+
+                console.log(req.body);
+                const {username, fullName, email, password} = req.body;
+                //validation
+                if(!username) return res.status(400).send("Username is required");
+            
+                if(!fullName) return res.status(400).send("Full Name is required");
+                if(!password || password.length < 6)
+                    return res
+                        .status(400)
+                        .send("Password is required and should be min 6 characters long");
+                // if(!(password == confirmPassword)) 
+                //     return res
+                //     .status(400).send("Passwords don't match. Please try again.");
+            
+                    let userExist = await User.findOne({email}).exec();
+                    if(userExist) return res.status(400).send("Email is taken");
+            
+                    let usernameExist = await User.findOne({username}).exec();
+                    if(usernameExist) return res.status(400).send("Username is taken");
+                    // let phoneNumberExist = await User.findOne({phoneNumber}).exec();
+                    //     if(phoneNumberExist) return res.status(400).send("An account with the following phone number already exists");
+                        //register
+                        const user = new User(req.body)
+
                 await user.save();
-                console.log("USER CREATED", user);
+                // console.log("USER CREATED", user);
                 return res.json({ok: true});
             } catch(err) {
                 console.log("CREATE USER FAILED", err)
@@ -53,21 +62,21 @@ export const login = async (req, res) => {
 
                 // Generating a token then sending it as a response to client.
                 
-                // let token = 
-                // jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-                //     expiresIn: "7d",
-                // });
-                // res.json({ token,
-                //     // Since we do not wanna send the whole user info, with the password, 
-                //     //user
-                //     user: {
-                //         _id: user._id,
-                //         name: user.name,
-                //         email: user.email,
-                //         createdAt: user.createdAt,
-                //         updatedAt: user.updatedAt,
-                //     }, 
-                // });
+                let token = 
+                jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+                    expiresIn: "7d",
+                });
+                res.json({ token,
+                    // Since we do not wanna send the whole user info, with the password, 
+                    //user
+                    user: {
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        createdAt: user.createdAt,
+                        updatedAt: user.updatedAt,
+                    }, 
+                });
         });
     } catch (error) {
         console.log("LOGIN ERROR", error);
