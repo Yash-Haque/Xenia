292a5eedbae240ae5ee11f580ee1b591b069be53
Yash-Haque <58705635+Yash-Haque@users.noreply.github.com>
2022-01-04 15:49:15 +0600

    Update hotel.js

--- client/src/actions/hotel.js
+++ client/src/actions/hotel.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 
-export const createHotel = async (token, data) =>
-await axios.post(`${process.env.REACT_APP_API}/create-hotel`, data,
-{
-    headers: {
-        Authorization: `Bearer ${token}`,
-    },
-});
+// The token Authorization prop has been commented out in order to verify whether the 
+// Functions were actually working or not. So to send token as a parameter, please pass
+// it before the already existing argument.
 
+// REACT_APP_API had been the default backend port.
+
+
+//token to be sent as a parameter of createHotel
+export const createHotel = async (data) =>
+await axios.post(`${process.env.REACT_APP_API}/create-hotel`, data
+// ,
+// {
+//     headers: {
+//         Authorization: `Bearer ${token}`,
+//     },
+// }
+);
+
+
+// For HomePage View
 export const allHotels = async () => await 
 axios.get(`${process.env.REACT_APP_API}/hotels`);
 
@@ -18,3 +30,50 @@ export const diffDays = (from, to) => {
     const difference = Math.round(Math.abs((start - end) / day));
     return difference;
 }
+
+//token to be sent as a parameter of sellerHotels
+export const sellerHotels = async () => 
+await axios.get(`${process.env.REACT_APP_API}/seller-hotels`
+// , {
+//     headers: {
+//         Authorization: `Bearer ${token}`,
+//     },
+// }
+);
+
+//token to be sent as a parameter of deleteHotel
+export const deleteHotel = async (hotelId) => 
+await axios.delete(
+    `${process.env.REACT_APP_API}/delete-hotel/${hotelId}`
+//     ,{    
+//         headers:{
+//         Authorization: `Bearer ${token}`,
+//     },
+// }
+);
+
+export const read = async (hotelId) => 
+await axios.get(`${process.env.REACT_APP_API}/hotel/${hotelId}`);
+
+//token to be sent as a parameter of createHotel
+export const updateHotel = async (data, hotelId) =>
+await axios.put(`${process.env.REACT_APP_API}/update-hotel/${hotelId}`, data
+// ,
+// {
+//     headers: {
+//         Authorization: `Bearer ${token}`,
+//     },
+// }
+);
+
+
+export const userHotelBookings = async () => 
+await axios.get(`${process.env.REACT_APP_API}/user-hotel-bookings`
+// ,
+// {
+//     headers: {
+//         Authorization: `Bearer ${token}`,
+//     },
+// }
+);
+
